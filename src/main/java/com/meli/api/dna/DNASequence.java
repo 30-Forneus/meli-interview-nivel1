237ff220f6499d20bff927798726de12345ff0e4
Roberto Capone <roberto.capone@enerminds.com>
2020-06-28 12:29:39 -0300

    [STEP 1] Load test data from resource files

--- src/main/java/com/meli/api/dna/DNASequence.java
+++ src/main/java/com/meli/api/dna/DNASequence.java
@@ -1,15 +1,26 @@
 package com.meli.api.dna;
 
-import lombok.Getter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
+@Value
 @RequiredArgsConstructor(staticName = "of")
-@Getter
 public class DNASequence {
-	@NonNull final String[] dna;
+	
+	@NonNull final String id;
+	@NonNull final JSONObject jsonObject;
+	public static final String DNA_JSON_PATH = "dna";
 	
 	boolean isMutant() {
 		return Validator.INSTANCE.validate(this);	
+	}
+	
+	public String[] getDna() {
+		JSONArray array = (JSONArray) jsonObject.get(DNASequence.DNA_JSON_PATH);
+		return (String[])array.stream().toArray(String[]::new);
 	}
 }
--- src/test/java/com/meli/api/dna/DNASequenceTest.java
+++ src/test/java/com/meli/api/dna/DNASequenceTest.java
@@ -1,29 +1,58 @@
 package com.meli.api.dna;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class DNASequenceTest {
-		
-	private String[] has = {"ATGCGA", "CAGTGC", "TTTTTT", "AGACGG", "GCGTCA", "TCACTG", "012210", "543345"};
-	private String[] hasVertical = {"ATGCGA", "CAGTGC", "TTATTT", "AGACGG", "GCATCA", "TCACTG", "012210", "543345"};
-	private String[] hasDiagonal = {"ATGCGA", "CAGTGC", "TTATTT", "AGAAGG", "GCGTCA", "TCACTG", "012210", "543345"};
-	private String[] hasInvertedDiagonal = {"ATGCGA", "CAGGGC", "TTGTTT", "AGACGG", "GCGTCA", "TCACTG", "012210", "543345"};
-	private String[] doesNotHave = {"ATGCGA", "CAGTGC", "TTATTT", "AGACGG", "GCGTCA", "TCACTG", "012210", "543345"};
+
+	private static final String FAKE_ID = "#FAKE_ID#";
+	private static final String RESOURCE_PATH = "src/test/resources/meliMatriz-%s_07x07.json";
+
+	private static final String HAS = "HAS";
+	private static final String DOES_NOT_HAVE = "DOES_NOT_HAVE";
+	private static final String HAS_DIAGONAL = "HAS_DIAGONAL";
+	private static final String HAS_VERTICAL = "HAS_VERTICAL";
+	private static final String INV_DIAGONAL = "INV_DIAGONAL";
+
+	private static JSONObject has;
+	private static JSONObject doesNotHave;
+	private static JSONObject hasDiagonal;
+	private static JSONObject hasVertical;
+	private static JSONObject hasInvertedDiagonal;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		has = loadJson(HAS);
+		doesNotHave = loadJson(DOES_NOT_HAVE);
+		hasDiagonal = loadJson(HAS_DIAGONAL);
+		hasVertical = loadJson(HAS_VERTICAL);
+		hasInvertedDiagonal = loadJson(INV_DIAGONAL);
+	}
+
+	private static JSONObject loadJson(String type) throws Exception {
+		final FileReader in = new FileReader(format(RESOURCE_PATH, type));
+		return (JSONObject) (new JSONParser().parse(in));
+	}
 
 	@Test
 	void testIsMutant() {
-		assertTrue(DNASequence.of(has).isMutant());
-		assertTrue(DNASequence.of(hasVertical).isMutant());
-		assertTrue(DNASequence.of(hasDiagonal).isMutant());
-		assertTrue(DNASequence.of(hasInvertedDiagonal).isMutant());
+		assertTrue(DNASequence.of(FAKE_ID, has).isMutant());
+		assertTrue(DNASequence.of(FAKE_ID, hasVertical).isMutant());
+		assertTrue(DNASequence.of(FAKE_ID, hasDiagonal).isMutant());
+		assertTrue(DNASequence.of(FAKE_ID, hasInvertedDiagonal).isMutant());
 	}
 
 	@Test
 	void testIsNotMutant() {
-		assertFalse(DNASequence.of(doesNotHave).isMutant());
+		assertFalse(DNASequence.of(FAKE_ID, doesNotHave).isMutant());
 	}
 
 }
