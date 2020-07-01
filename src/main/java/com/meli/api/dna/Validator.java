fa639050e58781879499590aa8ab1276f2887121
Roberto Capone <roberto.capone@enerminds.com>
2020-07-01 03:17:48 -0300

    [STEP 1] Checking DNA using stream API

--- src/main/java/com/meli/api/dna/Validator.java
+++ src/main/java/com/meli/api/dna/Validator.java
@@ -2,16 +2,21 @@ package com.meli.api.dna;
 
 import static com.meli.api.dna.DNAMutantSequences.valuesAsString;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 enum Validator {
 	INSTANCE;
 
-//	boolean containsAny(String[] targetDNA) {	
-//		return Arrays.asList(targetDNA).stream()
-//				.filter(targetSeq -> Arrays.asList(DNAMutantSequences.values()).stream()
-//						.anyMatch(mSeq -> targetSeq.contains(mSeq.toString())))
-//				.collect(Collectors.toList()).isEmpty();				
-//	}
+	// java 8+ flavor
+	boolean containsAny(String[] targetDNA) {
+		return !Arrays.asList(targetDNA).stream()
+				.filter(analysedSequence -> Arrays.asList(valuesAsString()).stream()
+						.anyMatch(mutantSequence -> analysedSequence.contains(mutantSequence.toString())))
+				.collect(Collectors.toList()).isEmpty();				
+	}
 
+	// java 7- flavor
 	boolean containsAny(String[] targetDNA, String[] dnaMutantSequences) {
 		for (int i = 0; i < targetDNA.length; i++) {
 			for (int j = 0; j < dnaMutantSequences.length; j++) {
@@ -29,7 +34,12 @@ enum Validator {
 		return containsAny(dna, valuesAsString())
 				|| containsAny(scanner.transposed(dna), valuesAsString())
 				|| containsAny(scanner.diagonal(dna), valuesAsString())
-				|| containsAny(scanner.diagonalOfMirrored(dna), valuesAsString());
+				|| containsAny(scanner.diagonalOfMirrored(dna), valuesAsString()); // java 7- flavor
+//		return containsAny(dna)
+//				|| containsAny(scanner.transposed(dna))
+//				|| containsAny(scanner.diagonal(dna))
+//				|| containsAny(scanner.diagonalOfMirrored(dna));	// java 8+ flavor
+
 	}
 
 }
--- src/test/java/com/meli/api/dna/ValidatorTest.java
+++ src/test/java/com/meli/api/dna/ValidatorTest.java
@@ -1,5 +1,6 @@
 package com.meli.api.dna;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 class ValidatorTest {
 	
 	private String[] has = {"ATGCGA", "CAGTGC", "TTTTTT", "AGACGG", "GCGTCA", "TCACTG"};
+	private String[] doesNotHave = {"ACGTACG", "TACGTAC", "CGTACGT", "GTACGTA", "ACGTACG", "TACGTAC", "CGTACGT"};
 	private Validator validator = Validator.INSTANCE;
 	private static String[] mutantSequences;
 	
@@ -19,6 +21,9 @@ class ValidatorTest {
 	@Test
 	void testContainsAny() {
 		assertTrue(validator.containsAny(has, mutantSequences));
+		assertTrue(validator.containsAny(has));
+		assertFalse(validator.containsAny(doesNotHave, mutantSequences));
+		assertFalse(validator.containsAny(doesNotHave));
 	}
 
 }
